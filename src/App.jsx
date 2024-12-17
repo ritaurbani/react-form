2b296efb93114817d58748713ad0e29fb6c2d795
ritaurbani <ritaurbani@gmail.com>
2024-12-17 20:53:34 +0100

    feat: cancel options

--- src/App.jsx
+++ src/App.jsx
@@ -28,41 +28,50 @@ function App() {
     setPosts(newArray)
     //RESET CAMPO INPUT PER IL TITOLO
     setPostTitle("")
+  };
+
+  const removeElement = (elementToRemove) => {
+    const newArray = posts.filter((curpost)=>curpost !== elementToRemove);
+    setPosts(newArray)
   }
+
+
 
   return (
     <>
     <div className='container'> 
-      <h2>Il mio Blog</h2> 
+      <h2 className='text-center mb-4'>Il mio Blog</h2> 
           
    {/* INPUT nuovo post */}
       <section>
       
         <form onSubmit={handlePostForm} >
           <div className='mb-3'>
-            <label htmlFor="titolo">Titolo articolo</label>
+            <label htmlFor="titolo">Cerca titolo articolo</label>
             <input type="text"
               className='form-control' 
               id='titolo' 
               value={postTitle} 
               onChange={(event) => setPostTitle(event.target.value)}/>
           </div>
-          <button type='submit' className='btn btn-success'>Submit</button>
+          <button type='submit' className='my-4 btn btn-success'>Submit</button>
         </form>
       </section>
 
-        
-        <ul>
+        {/* LIST */}
+        <ul className='list-group list-group-flush'>
           {
             posts.map((post, index) => (
-              <li key={index}>{post.title}</li>
-
+              <li className='list-group-item' key={index}>{post.title}
+                  <span></span>  
+                  {/* chiamiamo la funzione passando l elemento cliccato */}
+                <button className='mx-2 btn btn-outline-success btn-sm' onClick={()=>{removeElement(post)}}>Cancel</button>
+              </li>
             ))
           }
         </ul>
       
-      {/* controllo per confermare che aggiorna */}
-      <p>{postTitle}</p>
+      
     </div>  
     </>
   )
